77db9aa242c6e678c56cff96d8300ccf39a67a28
Junwei Zhang <zhangjw248@gmail.com>
2021-05-12 08:32:30 -0400

    first

---
+++ src/main/java/com/zale/admin/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.zale.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+
+
+@Controller
+public class IndexController {
+
+
+    /**
+     * 来登录页
+     * @return
+     */
+    @GetMapping(value = {"/","/login"})
+    public String loginPage(){
+
+        return "login";
+    }
+
+}
