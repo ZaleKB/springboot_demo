5502380ecd0ba6b44eeb3d2df591ee494b411637
zale <zhangjw248@gmail.com>
2021-07-19 07:02:39 -0400

    Junit5Test added

--- src/test/java/com/zale/admin/BootAdminApplicationTests.java
+++ src/test/java/com/zale/admin/BootAdminApplicationTests.java
@@ -1,5 +1,7 @@
 package com.zale.admin;
 
+import com.zale.admin.bean.User;
+import com.zale.admin.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,9 @@ class BootAdminApplicationTests {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    UserMapper userMapper;
+
     @Test
     void contextLoads() {
         //        jdbcTemplate.queryForObject("select * from account_tbl")
@@ -27,6 +32,12 @@ class BootAdminApplicationTests {
 
         log.info("数据源类型：{}",dataSource.getClass());
 
+    }
+
+    @Test
+    void testUserMapper(){
+        User user = userMapper.selectById(1L);
+        log.info("用户信息：{}",user);
     }
 
 }
